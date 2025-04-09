78a4b3daa6574bf6582e2291bce645065a0c463d
Sardorbek Pulatov <sardorbek.pulatov@vestiairecollective.com>
2025-04-09 15:04:38 +0200

    replace deprecated diagnostics

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,9 +4,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Start LazyGit
-vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
-
 -- Save
 vim.keymap.set("n", "<C-s>", ":w<CR>")
 
@@ -14,7 +11,9 @@ vim.keymap.set("n", "<C-s>", ":w<CR>")
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 
 -- Diagnostic
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1, float = true }) end,
+    { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1, float = true }) end,
+    { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
